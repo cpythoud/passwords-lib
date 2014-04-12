e15e4e7bd7f23f63bb55acc784084f301400f52b
Christophe Pythoud <git@pythoud.net>
2014-04-12 14:01:23 -0700

    Added SAFE variants for char sets (removed characters that can be confused with one another).

--- bin/print_some_pwds.groovy
+++ bin/print_some_pwds.groovy
@@ -35,3 +35,7 @@ factory.reset().setMainChars(CharacterSets.LOWER_CASES)
 factory.addAltCharGroup(CharacterSets.UPPER_CASES, 2, 2).addAltCharGroup(CharacterSets.DIGITS, 1, 3).addAltCharGroup(CharacterSets.EASY_SYMBOLS, 1, 2)
 printTen('12 chars, with 2 uppercase, 1 to 3 digits and 1 or 2 symbols', factory.create())
 
+// 16 chars, safe upper and lower cases, 1-3 digits, 1-2 symbols
+factory.reset().setMinMaxChars(16, 16).setMainChars(CharacterSets.SAFE_LOWER_CASES + CharacterSets.SAFE_UPPER_CASES)
+factory.addAltCharGroup(CharacterSets.SAFE_DIGITS, 1, 3).addAltCharGroup(CharacterSets.EASY_SYMBOLS, 1, 2)
+printTen('16 chars, safe upper and lower cases, 1-3 digits, 1-2 symbols', factory.create())
--- src/net/pythoud/passwords/CharacterSets.java
+++ src/net/pythoud/passwords/CharacterSets.java
@@ -9,4 +9,8 @@ public class CharacterSets {
     public static final String UPPER_CASES = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static final String DIGITS = "0123456789";
     public static final String EASY_SYMBOLS = "!@#$%&*()-=+;:,./?";
+
+    public static final String SAFE_LOWER_CASES = "abcdefghijkmnopqrstuvwxyz";
+    public static final String SAFE_UPPER_CASES = "ACDEFGHIJKLMNPQRSTUVWXYZ";
+    public static final String SAFE_DIGITS = "2345679";
 }
