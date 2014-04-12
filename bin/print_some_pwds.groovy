8f52a521fc04453a18c0fb1b27192198f0a863f1
Christophe Pythoud <git@pythoud.net>
2014-04-12 11:46:50 -0700

    Added print_some_pwds.groovy test script.

---
+++ bin/print_some_pwds.groovy
@@ -0,0 +1,21 @@
+import net.pythoud.passwords.PasswordMaker
+
+void printTen(String title, PasswordMaker pm) {
+    println(title)
+    (1..title.length()).each { print('-')}
+    print("\n")
+    (1..10).each {
+        println(pm.getPassword())
+    }
+    println()
+}
+
+println()
+
+// get default factory and print some 12 letters passwords
+def factory = PasswordMaker.getFactory()
+printTen('Default Factory', factory.create())
+
+// 16 chars with numbers
+factory.setMinMaxChars(16, 16).setMainChars(PasswordMaker.LOWER_CASES + PasswordMaker.UPPER_CASES + PasswordMaker.DIGITS)
+printTen('16 chars with digits', factory.create())
