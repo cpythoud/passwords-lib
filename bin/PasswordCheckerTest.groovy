f10a21e3198b71eae55887587b36ce089e142caf
Christophe Pythoud <git@pythoud.net>
2014-04-12 17:34:36 -0700

    Added tests with a more complex password checker.

--- bin/PasswordCheckerTest.groovy
+++ bin/PasswordCheckerTest.groovy
@@ -1,3 +1,4 @@
+import net.pythoud.passwords.CharacterSets
 import net.pythoud.passwords.PasswordChecker
 
 class PasswordCheckerTest extends GroovyTestCase {
@@ -49,6 +50,61 @@ class PasswordCheckerTest extends GroovyTestCase {
     void testBasicMissingChars() {
         PasswordChecker checker = PasswordChecker.getBasicPasswordChecker()
         assertFalse(checker.check('Testabcde'))
+        assertFalse(checker.isTooShort())
+        assertFalse(checker.isTooLong())
+        assertFalse(checker.containsIllegalCharacter())
+        assertTrue(checker.isCharacterTypeMissing())
+    }
+
+    PasswordChecker getComplexChecker() {
+        PasswordChecker.Factory factory = PasswordChecker.getFactory()
+        factory.setMinMaxCharCount(16, 32)
+        factory.addCharSet(CharacterSets.LOWER_CASES, 1).addCharSet(CharacterSets.UPPER_CASES, 1)
+        factory.addCharSet(CharacterSets.DIGITS, 1).addCharSet(CharacterSets.EASY_SYMBOLS, 1)
+        return factory.create()
+    }
+
+    void testComplexOK() {
+        PasswordChecker checker = getComplexChecker()
+        assertTrue(checker.check('jda;fAklVdkE3dk6as'))
+        assertTrue(checker.check('jda;fAklVdkE3dk6'))
+        assertTrue(checker.check('djljladS@SjfklSjk!Ejk34sDljSO9de'))
+    }
+
+    void testComplexFailTooShort() {
+        PasswordChecker checker = getComplexChecker()
+        assertFalse(checker.check('T@st7'))
+        assertTrue(checker.isTooShort())
+        assertFalse(checker.isTooLong())
+        assertFalse(checker.containsIllegalCharacter())
+        assertFalse(checker.isCharacterTypeMissing())
+
+        assertFalse(checker.check('I2e456789@12345'))
+    }
+
+    void testComplexFailTooLong() {
+        PasswordChecker checker = getComplexChecker()
+        assertFalse(checker.check('djljladSjSjfklSjklEjk34@sDljSO9defjaksgjklasdfj?'))
+        assertFalse(checker.isTooShort())
+        assertTrue(checker.isTooLong())
+        assertFalse(checker.containsIllegalCharacter())
+        assertFalse(checker.isCharacterTypeMissing())
+
+        assertFalse(checker.check('ThirthyThreeCharactersLongN0Good#'))
+    }
+
+    void testComplexFailIllegalChars() {
+        PasswordChecker checker = getComplexChecker()
+        assertFalse(checker.check('Test#1234@12345_67890'))
+        assertFalse(checker.isTooShort())
+        assertFalse(checker.isTooLong())
+        assertTrue(checker.containsIllegalCharacter())
+        assertFalse(checker.isCharacterTypeMissing())
+    }
+
+    void testComplexMissingChars() {
+        PasswordChecker checker = getComplexChecker()
+        assertFalse(checker.check('Test@bcdefghiklmno'))
         assertFalse(checker.isTooShort())
         assertFalse(checker.isTooLong())
         assertFalse(checker.containsIllegalCharacter())
